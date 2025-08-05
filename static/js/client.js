da8e9df4527cb1ee33c70be6463e781c09361fc6
Nicola Febbrari <febbrari@gmail.com>
2025-08-06 00:57:34 +0200

    Refactor Dockerfile commands for clarity and organization; add build instructions to note.txt

--- static/js/client.js
+++ static/js/client.js
@@ -1,11 +1,27 @@
 'use strict';
 
-$(document).on('online', function() {
+$(document).on('online', function () {
 	console.log('Application got online event, reloading');
 	window.location.reload();
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
 	console.log('Application got ready event');
 	window.Nightscout.client.init();
+
+	window.addEventListener('message', (event) => {
+		if (!event.origin.endsWith('cgmsim.com')) return;
+		if (event.data.type === 'SET_STORAGE') {
+			localStorage.setItem(event.data.key, event.data.value);
+			console.log(`Set ${event.data.key} = ${event.data.value}`);
+
+			event.source.postMessage(
+				{
+					type: 'STORAGE_SET_SUCCESS',
+					key: event.data.key,
+				},
+				event.origin,
+			);
+		}
+	});
 });
